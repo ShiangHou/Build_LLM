184abe0a3ef501a65a9eb4f976dd63f2842b0f7f
shangshang <1309063102@qq.com>
2026-06-10 00:01:31 +0800

    继续更新bpe

--- Homework/assignment1-basics/bpe.py
+++ Homework/assignment1-basics/bpe.py
@@ -75,59 +75,48 @@ def pair_counts(word_count:dict[tuple[int,...],int]) -> dict[tuple[int,int],int]
     return pairs
 
 #下一步，找到最多的字节对
-def get_most_frequent_pair
-
-
-
-def train_bpe(
-    input_path : str,
-    vocab_size :int,
-    special_tokens : list[str]
-    )->tuple[dict[int,bytes],list[tuple[bytes,bytes]]]:
+def get_most_frequent_pair(pair_counter:dict[tuple[int,int],int])->tuple[int,int]:
     '''
-    在这里复述一下bpe的整体的思路
-    1. 把语料转化为utf-8,加入special，构建基础词表
-    2. Pre-tokenization
-    3. 聚合，做一个词频映射表和 邻接对频次表
-    4. 贪心迭代
+    这里的输入就是pair_counts的输出，即那个[int,int]表示两个相邻的token对，后面的就是出现的总次数
+    这里想找到出现次数最多的那一个就行
+
+    方法是先找到频率最高的，然后
     '''
+    max_freq = max(pair_counter.values())#这个就是找到最大的次数是哪一个，因为pair是个字典
+    candidates = [pair for pair,freq in pair_counter.items() if freq == max_freq ]
 
-    #第一步是要把最后的输出给初始化出来
+    res = max(candidates)
+
+    return res#这里返回的是出现最多的字节对
 
 
-    # ==========================================
-    # 第一步：初始化状态 (Initialization)
-    # ==========================================
-    
-    #最后是输出了两个，一个vocab，即字符映射，这里就是0-255的字节
-    # 1. 基础词表：包含 256 个单字节 (0 ~ 255)
-    # 字典的键是 ID (int)，值是对应的单字节 (bytes)
-    vocab : dict[int,bytes] = {i : bytes([i]) for i in range(256)}
+
+def add_pair_to_vocab(
+        vocab:dict[int,bytes],
+        pair:tuple[int,int]
+)-> int:
     '''
-    知识点，这里的bytes是一种数据类型，比如说，bytes[65] 就是所谓的 A，
-    之前我们学到过，一共是256个，每一个数字都对应着一个字符串，这里就是直接用bytes这个来去把0-255的数字直接转为了对应的字符串
-    然后这里的i:bytes就是字典的key-value
+    这里的入参两个，一个是我们的词表，一个是刚才get_most_frequent_pair得到的字节对
+
     '''
 
-    # 2. 注册特殊 Token：分配从 256 开始的 ID
-    #这里就是把输入中的special_token直接转化为newtoken，然后给上新的id名称就是
+    index1,index2 = pair
+    vocab[len(vocab)] = vocab[index1]+vocab[index2]#如果一个是a，一个是b，那么这里就得到了ab这个的byte值
 
-    #这里就是一个简单的for循环，
-    new_token_id = 256 #新的从256开始
-    for token in special_tokens:
-        vocab[new_token_id] = token.encode('utf-8')#把token解码utf8然后存在表里
-        new_token_id += 1
-    
-
-    #初始化合并表格,最开始的时候是空
-    #合并表是一个list，里面存着两对，表示每一对都是两个byte类型
-    merge : list[tuple[bytes,bytes]] = []
-
-    # 3:读取path，进行预分词
+    return len(vocab)-1
     
 
 
+#下一步是更新文本中所有出现该字节对的地方，以及重新统计文本中所有相邻字节对的出现频率
+#需要遍历所有的word，看有没有出现，出现的化就去做
 
-    #最后就是需要return这两个
-    return vocab, merge
-
+def merge(
+        word_counter:dict[tuple[bytes] | tuple[int], int],
+        pair:tuple[int,int],
+        new_id:int
+)->tuple[dict[tuple[int], int], dict[tuple[int, int], int]]:
+    '''
+    pair是之前的get_most_frequent_pair找到的字节对
+    new_id
+    '''
+    pass
